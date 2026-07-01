a6d640f10a71a829dd3dc23ab816977173b3e5f6
eli <elijahtnrf@gmail.com>
2026-07-01 10:46:58 +0800

    resolve TFLite tensor allocation crash and listener bugs

--- frontend/android/app/src/main/java/com/heyroute/wakeword/WakeWordService.java
+++ frontend/android/app/src/main/java/com/heyroute/wakeword/WakeWordService.java
@@ -48,8 +48,6 @@ public class WakeWordService {
     private void loadModels() {
         try {
             melspectrogramInterpreter = new Interpreter(loadModelFile("melspectrogram.tflite"));
-            melspectrogramInterpreter.resizeInput(0, new int[]{1, CHUNK_SIZE}); // Dynamic to 1280
-            melspectrogramInterpreter.allocateTensors();
             
             embeddingInterpreter = new Interpreter(loadModelFile("embedding_model.tflite"));
             heyRouteInterpreter = new Interpreter(loadModelFile("hey_route.tflite"));
